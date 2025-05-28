bd0e5a5fe2e39bdd4b7abb76288a06e9e07b0405
Gurjot Singh <gurjotsingh2004@gmail.com>
2025-05-28 13:11:00 +0530

    Made changes to Main.jsx and App.css Created Section.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,17 @@ import {useState} from "react"
 export default function Main(){
     const [ingredients,setIngredients] = useState([]) 
     const List1 = ingredients.map((item)=>{
-        return <li key={item}>
+        return <li className="slide-in" key={item}>
             {item}
         </li>
     })
     
     function handleSubmit(formData){
-        const newIngredient = formData.get("ingredient")        
-        setIngredients((prev)=> [...prev,newIngredient])
+        const newIngredient = formData.get("ingredient") 
+        if(newIngredient.length>1){
+            setIngredients((prev)=> [...prev,newIngredient])
+        }       
+        
 
     }
 
@@ -24,10 +27,17 @@ export default function Main(){
             />
         <button>Add Ingredient</button>
     </form>
-        <ul>
-        {List1}
-        </ul>
-
+    {ingredients.length>0 && <section>
+        <h2 className="slide-in">Ingredients Added:</h2>
+        <ul className="ingredients-list" aria-live="polite">{List1}</ul>
+        {ingredients.length>2 &&<div className="get-recipe-container slide-in2">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+    </section>}
     </main>
     </>)
 }
